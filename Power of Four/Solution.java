c94841ef89bb878f2c3e1b85fddcb0f4d62e182c
Praveen019283 <praveen.shah@kooapp.com>
2022-08-22 10:20:06 +0530

    22 Aug

---
+++ Power of Four/Solution.java
@@ -0,0 +1,12 @@
+
+// Question : https://leetcode.com/problems/power-of-four/
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0) return false;
+        while(n%4==0){
+            n/=4;
+        }
+        return n==1;
+    }
+}
